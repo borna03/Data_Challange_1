06f34ae71c3a73fe7cccb1149c70229dcad051e5
Daan Stefens <d.j.e.stefens@student.tue.nl>
2023-05-09 15:42:57 +0200

    restructure 2.0

--- clean_data.py
+++ clean_data.py
@@ -1,10 +1,5 @@
 from config import *
 
-array = []
-for filename in os.listdir(directory):
-    array.append(filename)
-counter = 0
-
 
 def remove_nested_keys(dictionary, keys_to_remove):
     """
@@ -22,63 +17,69 @@ def remove_nested_keys(dictionary, keys_to_remove):
     return dictionary
 
 
-for file_name in array:
-    start_time = time.time()
-    counter += 1
+def clean():
+    array = []
+    for filename in os.listdir(directory):
+        array.append(filename)
+    counter = 0
 
-    with open(f'{directory}/{str(file_name)}') as f:
-        for line in f:
-            try:
-                data = json.loads(line)
-                remove_nested_keys(data,
-                                   ['id', 'source', 'url', 'description', 'protected', 'friends_count', 'listed_count',
-                                    'favourites_count', 'utc_offset', 'time_zone', 'contributors_enabled',
-                                    'is_translator', 'profile_background_color', 'profile_background_image_url',
-                                    'profile_background_image_url_https', 'profile_background_tile',
-                                    'profile_link_color', 'profile_sidebar_border_color', 'profile_sidebar_fill_color',
-                                    'profile_text_color', 'profile_use_background_image', 'profile_image_url',
-                                    'profile_image_url_https', 'profile_banner_url', 'default_profile',
-                                    'default_profile_image', 'following', 'follow_request_sent', 'notifications',
-                                    'geo', 'contributors', 'timestamp_ms', 'extended_entities',
-                                    'quoted_status_permalink', 'possibly_sensitive', 'retweeted', 'favorited',
-                                    'quoted_status', 'is_quote_status', 'quote_status_id', 'geo_enabled',
-                                    'in_reply_to_screen_name', 'in_reply_to_user_id', 'in_reply_to_status_id',
-                                    'truncated', 'media', 'urls', 'bounding_box', 'attributes', 'symbols', 'name',
-                                    'screen_name', 'translator_type'])
+    for file_name in array:
+        start_time = time.time()
+        counter += 1
+
+        with open(f'{directory}/{str(file_name)}') as f:
+            for line in f:
                 try:
-                    del(data['user']['created_at'])
-                    del(data['user']['lang'])
-                    del(data['retweeted_status']['display_text_range'])
-                    del(data['retweeted_status']['extended_tweet'])
-                    del(data['retweeted_status']['created_at'])
-                    del(data['retweeted_status']['text'])
-                    del(data['retweeted_status']['in_reply_to_status_id_str'])
-                    del(data['retweeted_status']['coordinates'])
-                    del(data['retweeted_status']['place'])
-                    del(data['retweeted_status']['quote_count'])
-                    del(data['retweeted_status']['reply_count'])
-                    del(data['retweeted_status']['retweet_count'])
-                    del(data['retweeted_status']['favorite_count'])
-                    del(data['retweeted_status']['entities'])
-                    del(data['retweeted_status']['filter_level'])
-                    del(data['retweeted_status']['user']['location'])
-                    del(data['retweeted_status']['user']['verified'])
-                    del(data['retweeted_status']['user']['followers_count'])
-                    del(data['retweeted_status']['user']['statuses_count'])
-                    del(data['retweeted_status']['user']['created_at'])
+                    data = json.loads(line)
+                    remove_nested_keys(data,
+                                       ['id', 'source', 'url', 'description', 'protected', 'friends_count', 'listed_count',
+                                        'favourites_count', 'utc_offset', 'time_zone', 'contributors_enabled',
+                                        'is_translator', 'profile_background_color', 'profile_background_image_url',
+                                        'profile_background_image_url_https', 'profile_background_tile',
+                                        'profile_link_color', 'profile_sidebar_border_color', 'profile_sidebar_fill_color',
+                                        'profile_text_color', 'profile_use_background_image', 'profile_image_url',
+                                        'profile_image_url_https', 'profile_banner_url', 'default_profile',
+                                        'default_profile_image', 'following', 'follow_request_sent', 'notifications',
+                                        'geo', 'contributors', 'timestamp_ms', 'extended_entities',
+                                        'quoted_status_permalink', 'possibly_sensitive', 'retweeted', 'favorited',
+                                        'quoted_status', 'is_quote_status', 'quote_status_id', 'geo_enabled',
+                                        'in_reply_to_screen_name', 'in_reply_to_user_id', 'in_reply_to_status_id',
+                                        'truncated', 'media', 'urls', 'bounding_box', 'attributes', 'symbols', 'name',
+                                        'screen_name', 'translator_type'])
                     try:
-                        for obj_numb in range(0, 6):
-                            del (data['entities']['user_mentions'][obj_numb]['screen_name'])
-                            del (data['entities']['user_mentions'][obj_numb]['name'])
-                            del (data['entities']['user_mentions'][obj_numb]['id'])
+                        del(data['user']['created_at'])
+                        del(data['user']['lang'])
+                        del(data['retweeted_status']['display_text_range'])
+                        del(data['retweeted_status']['extended_tweet'])
+                        del(data['retweeted_status']['created_at'])
+                        del(data['retweeted_status']['text'])
+                        del(data['retweeted_status']['in_reply_to_status_id_str'])
+                        del(data['retweeted_status']['coordinates'])
+                        del(data['retweeted_status']['place'])
+                        del(data['retweeted_status']['quote_count'])
+                        del(data['retweeted_status']['reply_count'])
+                        del(data['retweeted_status']['retweet_count'])
+                        del(data['retweeted_status']['favorite_count'])
+                        del(data['retweeted_status']['entities'])
+                        del(data['retweeted_status']['filter_level'])
+                        del(data['retweeted_status']['user']['location'])
+                        del(data['retweeted_status']['user']['verified'])
+                        del(data['retweeted_status']['user']['followers_count'])
+                        del(data['retweeted_status']['user']['statuses_count'])
+                        del(data['retweeted_status']['user']['created_at'])
+                        try:
+                            for obj_numb in range(0, 6):
+                                del (data['entities']['user_mentions'][obj_numb]['screen_name'])
+                                del (data['entities']['user_mentions'][obj_numb]['name'])
+                                del (data['entities']['user_mentions'][obj_numb]['id'])
+                        except:
+                            pass
                     except:
                         pass
-                except:
-                    pass
-                collection.insert_one(data)
-            except json.decoder.JSONDecodeError as err:
-                print("JSONDecodeError:", err)
+                    collection.insert_one(data)
+                except json.decoder.JSONDecodeError as err:
+                    print("JSONDecodeError:", err)
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print("Time: ", elapsed_time, file_name)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Time: ", elapsed_time, file_name)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from config import *
 import plot_figures
 import clean_data
+
+

f136644c6852c9dfdcc3bd386294b33594c0ff9a
Daan Stefens <d.j.e.stefens@student.tue.nl>
2023-05-09 22:37:07 +0200

    Finished code for getting the data for the lang proportion pie charts (tweeted_at_lang, responded_to_lang)

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from config import *
 import plot_figures
 import clean_data
 
-
+resp_langs = plot_figures.responded_at_lang(airlines['VirginAtlantic']['id_str'])
+resp_langs_sort = dict(sorted(resp_langs.items(), key=lambda item: item[1], reverse=True))
+print(resp_langs_sort)
--- plot_figures.py
+++ plot_figures.py
@@ -1,7 +1,5 @@
 from config import *
 from datetime import datetime
-from datetime import datetime
-from collections import defaultdict
 
 
 def tweet_count(id_str):
@@ -12,91 +10,27 @@ def tweeted_at_count(id_str):
     return collection.count_documents({'entities.user_mentions': {'$elemMatch': {'id_str': id_str}}})
 
 
-
-def tweeted_at_count_month(chosen_airline_id):
-    tweet_count_per_month = {}
-
-    for doc in collection.find({
-        'entities.user_mentions': {
-            '$elemMatch': {'id_str': chosen_airline_id}
-        }
-    }):
-        created_at = doc['created_at']
-        created_at_date = datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")  # Parse the date string
-        if created_at_date.year == 2019 and 6 <= created_at_date.month <= 12:
-            month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
-            tweet_count_per_month.setdefault(month_year, 0)
-            tweet_count_per_month[month_year] += 1
-        elif created_at_date.year == 2020 and created_at_date.month <= 3:
-            month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
-            tweet_count_per_month.setdefault(month_year, 0)
-            tweet_count_per_month[month_year] += 1
-
-    return tweet_count_per_month
-
-chosen_airline_id = "20626359"
-tweet_count_per_month = tweeted_at_count_month(chosen_airline_id)
-
-print("Tweet count per month:")
-for month_year, count in tweet_count_per_month.items():
-    print(f"Month: {month_year}, Count: {count}")
+def tweeted_at_lang(id_str):
+    langs = {}
+    for tweet in collection.find({'entities.user_mentions': {'$elemMatch': {'id_str': id_str}}}):
+        if tweet['lang'] in langs:
+            langs[tweet['lang']] += 1
+        else:
+            langs[tweet['lang']] = 1
+    return langs
 
 
-def replied_at_count_month(chosen_airline_id):
-    reply_count_per_month = {}
+def responded_to_lang(id_str):
+    langs = {}
+    responses = [response for response in
+                 collection.find({'user.id_str': id_str, 'in_reply_to_status_id_str': {'$ne': None}},
+                                 {'in_reply_to_status_id_str': 1, "_id": 0}).distinct('in_reply_to_status_id_str')]
 
-    for doc in collection.find({
-        'entities.user_mentions': {
-            '$elemMatch': {'id_str': chosen_airline_id}
-        }
-    }):
-        if doc['in_reply_to_user_id_str'] == chosen_airline_id:
-            created_at = doc['created_at']
-            created_at_date = datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")  # Parse the date string
-            if created_at_date.year == 2019 and 6 <= created_at_date.month <= 12:
-                month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
-                reply_count_per_month.setdefault(month_year, 0)
-                reply_count_per_month[month_year] += 1
-            elif created_at_date.year == 2020 and created_at_date.month <= 3:
-                month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
-                reply_count_per_month.setdefault(month_year, 0)
-                reply_count_per_month[month_year] += 1
-
-    print("Reply count per month:")
-    for month_year, count in reply_count_per_month.items():
-        print(f"Month: {month_year}, Count: {count}")
-
-    return reply_count_per_month
-
-chosen_airline_id = "20626359"
-reply_count_per_month = replied_at_count_month(chosen_airline_id)
-
-def tweet_and_reply_count_month(chosen_airline_id):
-    mention_count_per_month = {}
-    reply_count_per_month = {}
-
-    for doc in collection.find({
-        'entities.user_mentions': {
-            '$elemMatch': {'id_str': chosen_airline_id}
-        }
-    }):
-        created_at = doc['created_at']
-        created_at_date = datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")  # Parse the date string
-        if (created_at_date.year == 2019 and 6 <= created_at_date.month <= 12) or (created_at_date.year == 2020 and created_at_date.month <= 3):
-            month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
-            mention_count_per_month.setdefault(month_year, 0)
-            mention_count_per_month[month_year] += 1
-
-            if doc['in_reply_to_user_id_str'] == chosen_airline_id:
-                reply_count_per_month.setdefault(month_year, 0)
-                reply_count_per_month[month_year] += 1
-
-    print("Mention and Reply count per month:")
-    for month_year, mention_count in mention_count_per_month.items():
-        reply_count = reply_count_per_month.get(month_year, 0)
-        print(f"Month: {month_year}, Mention Count: {mention_count}, Reply Count: {reply_count}")
-
-    return mention_count_per_month, reply_count_per_month
-
-chosen_airline_id = "20626359"
-mention_count_per_month, reply_count_per_month = tweet_and_reply_count_month(chosen_airline_id)
+    for tweet in collection.find({'entities.user_mentions': {'$elemMatch': {'id_str': id_str}},
+                                  'in_reply_to_status_id_str': None}, {'id_str': 1, 'lang': 1, '_id': 0}):
+        if tweet['id_str'] in responses:
+            if tweet['lang'] in langs:
+                langs[tweet['lang']] += 1
+            else:
+                langs[tweet['lang']] = 1
+    return langs

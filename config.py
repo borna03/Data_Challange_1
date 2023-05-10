a36c60cd2ded62db4159917c896c958476e980e5
Daan Stefens <d.j.e.stefens@student.tue.nl>
2023-05-10 17:32:53 +0200

    Finished all plotting code

--- config.py
+++ config.py
@@ -2,14 +2,12 @@ import pymongo
 import json
 import os
 import time
-import matplotlib as plt
-import pandas
-import numpy as np
+
 
 client = pymongo.MongoClient("mongodb://localhost:27017")
-db = client['DBL_test']
+db = client['DBL_data']
 collection = db['data']
-directory = "C:/Users/mattl/OneDrive/Desktop/data"
+directory = "C:/DBL_Data_Challenge/All Data/data"
 airlines = {'KLM': {'id_str': '56377143', 'tweet_count': 36221, 'tweeted_at_count': 236089},
             'AirFrance': {'id_str': '106062176', 'tweet_count': 10076, 'tweeted_at_count': 96299},
             'British_Airways': {'id_str': '18332190', 'tweet_count': 113289, 'tweeted_at_count': 564278},
@@ -23,5 +21,4 @@ airlines = {'KLM': {'id_str': '56377143', 'tweet_count': 36221, 'tweeted_at_coun
             'Qantas': {'id_str': '218730857', 'tweet_count': 12863, 'tweeted_at_count': 168063},
             'EtihadAirways': {'id_str': '45621423', 'tweet_count': 1513, 'tweeted_at_count': 80441},
             'VirginAtlantic': {'id_str': '20626359', 'tweet_count': 22813, 'tweeted_at_count': 163544}}
-
 
--- plot_figures.py
+++ plot_figures.py
@@ -1,7 +1,8 @@
 from config import *
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 from datetime import datetime
-from datetime import datetime
-from collections import defaultdict
 
 
 def tweet_count(id_str):
@@ -35,33 +36,24 @@ def tweeted_at_count_month(chosen_airline_id):
 
 
 def replied_at_count_month(chosen_airline_id):
+    print('Starting function')
     reply_count_per_month = {}
-
-    # Search for tweets where our airline was mentioned and retrieve their tweet IDs
-    mention_docs = collection.find({
-        'entities.user_mentions': {
-            '$elemMatch': {'id_str': chosen_airline_id}
-        }
-    }, {'id_str': 1})
-
-    # Collect tweet IDs in a list
-    mention_tweet_ids = [doc['id_str'] for doc in mention_docs]
-
-    # Find tweets from our airline that are replies to the mentioned tweets
-    reply_docs = collection.find({
-        'user.id_str': chosen_airline_id,
-        'in_reply_to_status_id_str': {'$in': mention_tweet_ids}
-    })
-
-    # Iterate over reply docs and count them per month
-    for reply_doc in reply_docs:
-        created_at = reply_doc['created_at']
-        created_at_date = datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")  # Parse the date string
-        if (created_at_date.year == 2019 and 6 <= created_at_date.month <= 12) or (
-                created_at_date.year == 2020 and created_at_date.month <= 3):
-            month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
-            reply_count_per_month.setdefault(month_year, 0)
-            reply_count_per_month[month_year] += 1
+    responses = [response for response in
+                 collection.find({'user.id_str': chosen_airline_id, 'in_reply_to_status_id_str': {'$ne': None}},
+                                 {'in_reply_to_status_id_str': 1, "_id": 0}).distinct('in_reply_to_status_id_str')]
+    print('Found responses')
+    for tweet in collection.find({'entities.user_mentions': {'$elemMatch': {'id_str': chosen_airline_id}},
+                                  'in_reply_to_status_id_str': None}, {'id_str': 1, 'created_at': 1, '_id': 0}):
+        if tweet['id_str'] in responses:
+            print('Found tweet')
+            created_at = tweet['created_at']
+            created_at_date = datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")  # Parse the date string
+            if (created_at_date.year == 2019 and 6 <= created_at_date.month <= 12) or (
+                    created_at_date.year == 2020 and created_at_date.month <= 3):
+                month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
+                reply_count_per_month.setdefault(month_year, 0)
+                reply_count_per_month[month_year] += 1
+            print('Tweet handled')
 
     return reply_count_per_month
 
@@ -90,3 +82,86 @@ def responded_to_lang(id_str):
             else:
                 langs[tweet['lang']] = 1
     return langs
+
+
+def plot_bar_chart():
+    # creating dataframe
+    df = pd.DataFrame({
+        'Airline': airlines.keys(),
+        'Tweets': [airlines[airline]['tweet_count'] for airline in airlines],
+        'Tweeted at': [airlines[airline]['tweeted_at_count'] for airline in airlines]
+    })
+
+    # plotting graph
+    df.plot(x="Airline", y=["Tweets", "Tweeted at"], kind="bar")
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+    plt.title("Amount of tweets tweeted by and at airlines")
+    plt.ylabel('Amount')
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_line_plot(tweet_count_per_month_us, reply_count_per_month_us, tweet_count_per_month_them,
+                   reply_count_per_month_them):
+    percentage_dict_our_airline = {}
+    percentage_dict_competitor_airline = {}
+
+    for tweet, reply in zip(tweet_count_per_month_us, reply_count_per_month_us):
+        ratio = reply_count_per_month_us[reply] / tweet_count_per_month_us[tweet] * 100
+        percentage_dict_our_airline[tweet] = ratio
+
+    for tweet, reply in zip(tweet_count_per_month_them, reply_count_per_month_them):
+        ratio = reply_count_per_month_them[reply] / tweet_count_per_month_them[tweet] * 100
+        percentage_dict_competitor_airline[tweet] = ratio
+
+    # Extract x-axis (keys) and y-axis (values) from the dictionaries
+    x_values = list(percentage_dict_our_airline.keys())
+    y_values1 = list(percentage_dict_our_airline.values())
+    y_values2 = list(percentage_dict_competitor_airline.values())
+
+    # Plot the line plots
+    plt.plot(x_values, y_values1, marker='o', label='VirginAtlantic')
+    plt.plot(x_values, y_values2, marker='o', label='British_Airways')
+
+    plt.xlabel('Month')
+    plt.ylabel('Percentage of replies')
+    plt.title('Line Plot of replies from VirginAtlantic')
+    plt.legend()
+
+    # Rotate x-axis labels if needed
+    plt.xticks(rotation=30)
+
+    plt.show()
+
+
+def plot_nested_pie(values):
+    fig, ax = plt.subplots()
+
+    size = 0.4
+    vals = np.array(values)
+
+    outer_colors = ['royalblue', 'forestgreen', 'firebrick']
+    inner_colors = ['silver', 'gold']
+
+    ax.pie(vals.sum(axis=1), radius=1, colors=outer_colors,
+           wedgeprops=dict(width=size, edgecolor='w'))
+
+    ax.pie(vals.flatten(), radius=1 - size, colors=inner_colors,
+           wedgeprops=dict(width=size, edgecolor='w'))
+
+    ax.set(aspect="equal", title='Nested Pie Chart')
+
+    # Combine outer and inner labels
+    outer_labels = ['English', 'Undefined', 'ROW']
+    inner_labels = ['Ignored', 'Replied']
+    all_labels = outer_labels + inner_labels
+
+    # Combine outer and inner colors
+    all_colors = outer_colors + inner_colors
+
+    # Add legend
+    ax.legend(all_labels, title="Legend", loc="upper right", bbox_to_anchor=(0.2, 1))
+
+    ax.set(aspect="equal", title='Replies in different languages (British Airways)')
+
+    plt.show()

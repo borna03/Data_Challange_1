67bc5da8320d3bd4b1863eb77aef694cf02c9958
Matthew <s.lee2@student.tue.nl>
2023-05-10 14:18:37 +0200

    fix the reply_at_count_month function for the plot later

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from config import *
 import plot_figures
 import clean_data
 
-resp_langs = plot_figures.responded_at_lang(airlines['VirginAtlantic']['id_str'])
-resp_langs_sort = dict(sorted(resp_langs.items(), key=lambda item: item[1], reverse=True))
-print(resp_langs_sort)
+chosen_airline_id = "20626359"
+tweet_count_per_month = plot_figures.tweeted_at_count_month(chosen_airline_id)
+reply_count_per_month = plot_figures.replied_at_count_month(chosen_airline_id)
+
+print("Tweet count per month:")
+for month_year, count in tweet_count_per_month.items():
+    print(f"Month: {month_year}, Count: {count}")
+
+print("Reply count per month:")
+for month_year, count in reply_count_per_month.items():
+    print(f"Month: {month_year}, Count: {count}")
+
+print(plot_figures.tweeted_at_lang(airlines['VirginAtlantic']['id_str']))
+print(plot_figures.responded_to_lang(airlines['VirginAtlantic']['id_str']))
--- plot_figures.py
+++ plot_figures.py
@@ -1,5 +1,7 @@
 from config import *
 from datetime import datetime
+from datetime import datetime
+from collections import defaultdict
 
 
 def tweet_count(id_str):
@@ -8,6 +10,60 @@ def tweet_count(id_str):
 
 def tweeted_at_count(id_str):
     return collection.count_documents({'entities.user_mentions': {'$elemMatch': {'id_str': id_str}}})
+
+
+def tweeted_at_count_month(chosen_airline_id):
+    tweet_count_per_month = {}
+
+    for doc in collection.find({
+        'entities.user_mentions': {
+            '$elemMatch': {'id_str': chosen_airline_id}
+        }
+    }):
+        created_at = doc['created_at']
+        created_at_date = datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")  # Parse the date string
+        if created_at_date.year == 2019 and 6 <= created_at_date.month <= 12:
+            month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
+            tweet_count_per_month.setdefault(month_year, 0)
+            tweet_count_per_month[month_year] += 1
+        elif created_at_date.year == 2020 and created_at_date.month <= 3:
+            month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
+            tweet_count_per_month.setdefault(month_year, 0)
+            tweet_count_per_month[month_year] += 1
+
+    return tweet_count_per_month
+
+
+def replied_at_count_month(chosen_airline_id):
+    reply_count_per_month = {}
+
+    # Search for tweets where our airline was mentioned and retrieve their tweet IDs
+    mention_docs = collection.find({
+        'entities.user_mentions': {
+            '$elemMatch': {'id_str': chosen_airline_id}
+        }
+    }, {'id_str': 1})
+
+    # Collect tweet IDs in a list
+    mention_tweet_ids = [doc['id_str'] for doc in mention_docs]
+
+    # Find tweets from our airline that are replies to the mentioned tweets
+    reply_docs = collection.find({
+        'user.id_str': chosen_airline_id,
+        'in_reply_to_status_id_str': {'$in': mention_tweet_ids}
+    })
+
+    # Iterate over reply docs and count them per month
+    for reply_doc in reply_docs:
+        created_at = reply_doc['created_at']
+        created_at_date = datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")  # Parse the date string
+        if (created_at_date.year == 2019 and 6 <= created_at_date.month <= 12) or (
+                created_at_date.year == 2020 and created_at_date.month <= 3):
+            month_year = created_at_date.strftime('%Y-%m')  # Format the date as "YYYY-MM"
+            reply_count_per_month.setdefault(month_year, 0)
+            reply_count_per_month[month_year] += 1
+
+    return reply_count_per_month
 
 
 def tweeted_at_lang(id_str):

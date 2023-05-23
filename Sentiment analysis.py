ab661cfa3b250d3cccb07fc6a55fe64b8d980132
Borna Simic <b.simic@student.tue.nl>
2023-05-23 21:34:08 +0200

    Code improvements in Sentiment analysis.py, soo that it can be used in the future.

--- Sentiment analysis.py
+++ Sentiment analysis.py
@@ -1,48 +1,40 @@
 import csv
-from textblob import TextBlob
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
 import numpy as np
 
-# Load model and tokenizer
-roberta = "cardiffnlp/twitter-roberta-base-sentiment"
-model = AutoModelForSequenceClassification.from_pretrained(roberta)
-tokenizer = AutoTokenizer.from_pretrained(roberta)
-labels = ['negative', 'neutral', 'positive']
-
 def load_csv_line_by_line(file_path):
-    counter = 0
-    dct = dict()
+    dct = {}
+
+    # Load model and tokenizer
+    roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+    model = AutoModelForSequenceClassification.from_pretrained(roberta)
+    tokenizer = AutoTokenizer.from_pretrained(roberta)
+    labels = ['negative', 'neutral', 'positive']
 
     # Read CSV file line by line
     with open(file_path, 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
-        for line in reader:
+        for counter, line in enumerate(reader):
             if counter != 0:
-                dct[counter] = [line[1], line[10]]  # Store relevant data in dictionary
-            counter += 1
+                tweet = line[10]
+                encoded_tweet = tokenizer(tweet, return_tensors='pt')
+                output = model(**encoded_tweet)
 
-    # Process each tweet in the dictionary
-    for key, value in dct.items():
-        tweet = value[1]
-        encoded_tweet = tokenizer(tweet, return_tensors='pt')
-        output = model(**encoded_tweet)
+                scores = output.logits.detach().numpy()[0]
+                scores = softmax(scores)  # Apply softmax to obtain probability distribution
 
-        scores = output[0][0].detach().numpy()
-        scores = softmax(scores)  # Apply softmax to obtain probability distribution
+                sentiment_label = labels[np.argmax(scores)]
 
-        dct[key].append(labels[np.argmax(scores)])  # Add predicted sentiment label to the dictionary
+                dct[counter] = [line[1], tweet, sentiment_label]  # Store relevant data in dictionary
+
 
     # Print the processed data
-    for key, value in dct.items():
-        print(key, value)
+    #for key, value in dct.items():
+        #print(key, value)
 
     # Calculate and print the accuracy
-    correct_counter = 0
-    for key, value in dct.items():
-        if value[0] == value[2]:  # Compare actual and predicted labels
-            correct_counter += 1
-
+    correct_counter = sum(1 for key, value in dct.items() if value[0] == value[2])
     accuracy = correct_counter / len(dct)
     print(accuracy)
 

721f52f29126258af40dd31651a6079fc5fdbe9a
Daan Stefens <d.j.e.stefens@student.tue.nl>
2023-06-13 17:18:00 +0200

    Again changed the code, ONLY ADD_REPLIES IS FINAL

--- create_conversations.py
+++ create_conversations.py
@@ -1,35 +1,62 @@
 from pymongo import UpdateOne, UpdateMany
 from data_load import *
 import pandas as pd
+import heapq
+import time
 
 
 # Function to (re)set a column
 def set_new_column(column_name, column_value):
+    print('Cleaning/Creating ' + column_name + " column")
     update_operation = {"$set": {column_name: column_value}}
     collection.update_many({}, update_operation)
-    print('done')
+    print('Done')
 
 
 # Add the replies to each tweet to the database
 def add_replies():
+    # Clean/Create the replies column
+    set_new_column('replies', [])
+
+    # Initialise needed variables
     tweets = collection.find()
+    replies = {}
     bulk_updates = []
 
-    # Iterate over your documents and define the update operation for each document
+    # Start timer
+    print('Starting calculation')
+    start_time = time.time()
+
+    # Iterate over documents and make a list containing for every tweet the tweets that reply to them
     count = 0
     for tweet in tweets:
+        # See if the tweet is a reply and add it to the tweet that it is a reply to
         if tweet['in_reply_to_status_id_str'] is not None:
-            filter = {"id_str": tweet["in_reply_to_status_id_str"]}  # Modify the filter based on your criteria
-            update_operation = {"$push": {"replies": {"$each": [tweet['id_str']]}}}
-            update = UpdateOne(filter, update_operation)
-            bulk_updates.append(update)
+            replies.setdefault(tweet['in_reply_to_status_id_str'], []).append(
+                tweet['id_str'])
         count += 1
-        print(count)
+        if count % 10000 == 0:
+            print('Amount of documents handled: ' + str(count))
+
+    # Define all update operations needed
+    for key, value in replies.items():
+        filter = {"id_str": {"$eq": key}}
+        update_operation = {"$push": {"replies": {"$each": value}}}
+        update = UpdateOne(filter, update_operation)
+        bulk_updates.append(update)
 
     # Perform the bulk write operation
-    print('doing the bulk update now')
+    print('Doing the bulk update now')
+    start_time_write = time.time()
     collection.bulk_write(bulk_updates)
-    print('done')
+    end_time = time.time()
+    calc_time = start_time_write - start_time
+    write_time = end_time - start_time_write
+    total_time = end_time - start_time
+    print(f"Calc time: {calc_time} seconds")
+    print(f"Write time: {write_time} seconds")
+    print(f"Total time: {total_time} seconds")
+    print('Done')
 
 
 # Function to create the sub-conversations
@@ -107,40 +134,81 @@ def levels_count(tweet_chain, id_str, level):
 
 # Add the general conversation id
 def general_conversation():
-    # Get all root tweets
-    root_tweets = collection.find({'in_reply_to_status_id_str': None})
+    # Clean/Create the conversation_id column
+    set_new_column('conversation_id', 0)
 
-    # Initialise conversation_id, and bulk_updates
-    conversation_id = 1
+    # Get all tweets
+    print('Getting tweets')
+    tweets = collection.find()
+
+    # Initialise needed variables
+    conversations = {}
+    seen_tweets = []
     bulk_updates = []
+    conversation_id = 1
+    counter = 0
+    start_time = time.time()
+    tweet_time = start_time
 
-    # Loop over all root tweets
-    for tweet in root_tweets:
-        # Query all replies in the chain
-        conversation = collection.aggregate([
-            {"$match": {"id_str": tweet['id_str']}},
-            {
-                "$graphLookup": {
-                    "from": "conversations",
-                    "startWith": "$id_str",
-                    "connectFromField": "id_str",
-                    "connectToField": "replies",
-                    "as": "reply_chain"
-                }
-            }
-        ])
+    # Evaluate every tweet and its replies, and add it to the dictionary
+    for tweet in tweets:
+        # Initialise tracking variables
+        active_tweets = tweet['replies'] + [tweet['id_str']]
+        unseen_tweets = sorted(active_tweets)
+        matching_keys = []
 
-        # Convert to a list of id's
-        id_list = [tweet['id_str'] for tweet in conversation]
+        # Check if we've already handled one of the tweets/replies and get its conversation_id
+        for id_str in active_tweets:
+            if id_str in seen_tweets:
+                keys = [key for key, value in conversations.items() if tweet['id_str'] in value]
+                matching_keys.extend(list(set(keys) - set(matching_keys)))
+                unseen_tweets.remove(id_str)
 
-        # Add an update operation to bulk_updates
-        filter = {"id_str": {"$in": id_list}}
-        update_operation = {"$set": {"conversation_id": conversation_id}}
+        # If we've not seen the tweets before make a new entry and up the conversation_id
+        if len(matching_keys) == 0:
+            conversations[conversation_id] = active_tweets
+            conversation_id += 1
+
+        # If one of the tweets is in another conversation, add all active tweets to that conversation
+        elif len(matching_keys) == 1:
+            conversations[matching_keys[0]].extend(list(set(active_tweets) - set(conversations[matching_keys[0]])))
+        elif len(matching_keys) > 1:
+            seen_id = matching_keys[0]
+            conversations[seen_id].extend(list(set(active_tweets) - set(conversations[seen_id])))
+            for key in matching_keys[1:]:
+                conversations[seen_id].extend(list(set(conversations[key]) - set(conversations[seen_id])))
+                del conversations[key]
+
+        # Add previously unseen tweets to the seen_tweets list
+        seen_tweets = list(heapq.merge(seen_tweets, unseen_tweets))
+        counter += 1
+        if counter % 10000 == 0:
+            current_time = time.time()
+            handle_time = current_time - tweet_time
+            tweet_time = current_time
+            print(f"Batch handle time: {handle_time} seconds")
+            print('Handled tweets: ' + str(counter))
+
+    # Define all update operations needed
+    print('Adding update operations')
+    start_write_time = time.time()
+    for key, value in conversations.items():
+        filter = {"id_str": {"$in": value}}
+        update_operation = {"$set": {"conversation_id": key}}
         update = UpdateMany(filter, update_operation)
         bulk_updates.append(update)
 
-        # Up the conversation_id
-        conversation_id += 1
-
-    # Perform all updates in bulk_updates
+    # Perform bulk update
+    print('Performing updates')
     collection.bulk_write(bulk_updates)
+    end_time = time.time()
+    calc_time = start_write_time - start_time
+    write_time = end_time - start_write_time
+    total_time = end_time - start_time
+    print(f"Calc time: {calc_time} seconds")
+    print(f"Write time: {write_time} seconds")
+    print(f"Total time: {total_time} seconds")
+    print('Done')
+
+
+
